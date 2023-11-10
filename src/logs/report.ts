857853cf8a9fd0a9c8946ff0dc7814b4858ee5ed
lihongyi <lihongyi@wangxiaobao.com>
2023-11-10 09:27:39 +0800

    fix: 合并参数

--- src/logs/report.ts
+++ src/logs/report.ts
@@ -47,9 +47,9 @@ export async function requestReportLog(
   if (customFields && !isObject(customFields)) {
     const msg = '埋点error:「customFields」必须是一个对象'
     err(msg)
-    return Promise.reject(msg)
+  } else {
+    extendFields = { ...extendFields, ...getCustomFields(customFields) }
   }
-  extendFields = getCustomFields(customFields)
 
   const { networkType } = await wxb.getNetworkType()
   requestId = `${Date.now()}_${uniqueId}`
